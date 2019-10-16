0fc0fbf20b0bd45157a4124f0b19b15b4f2e6b68
Zachary Piazza <za.piazza@gmail.com>
2019-10-16 18:34:44 -0400

    numbers_to_words() as a script using functions from phone_to_words_fx.py, updates in phone_to_words_fx.py

--- number_to_words.py
+++ number_to_words.py
@@ -1,134 +1,34 @@
-#this code pertains to the following assignment:
-#create a function number_to_words(), which takes as an argument a string representing a US phone
-#number and which outputs a string which has transformed part or all of the phone
-#number into a single "wordified" phone number that can be typed on a US telephone (for
-#example, a valid output of number_to_words("1-800-724-6837") could be
-#"1-800-PAINTER"). If you find it makes things simpler, feel free to constrain this function
-#to only output "wordifications" in English.
+# this code is related to the following assignment:
+# create a function number_to_words(), which takes as an argument a string representing a US phone
+# number and which outputs a string which has transformed part or all of the phone
+# number into a single "wordified" phone number that can be typed on a US telephone (for
+# example, a valid output of number_to_words("1-800-724-6837") could be
+# "1-800-PAINTER"). If you find it makes things simpler, feel free to constrain this function
+# to only output "wordifications" in English.
 
-#this version successfully locates PAINTER
-#this version only looks ofr 7-letter words
-#this version returns no solution for any number with a 0 or 1
-
-import re
-import itertools
-import nltk
-nltk.download('words')
-from nltk.corpus import words  # list of all words in the american english language
-# https://www.datasciencebytes.com/bytes/2014/11/03/get-a-list-of-all-english-words-in-python/
-# must install ntlk for this code to work
-# https://www.nltk.org/
-# sudo apt-get install python-numpy python-nltk
-
-# a dictionary mapping digits with corresponding phone-letters
-alphanumeric_dict = {"0": None,
-                     "1": None,
-                     "2": ('A','B','C'),
-                     "3": ('D','E','F'),
-                     "4": ('G','H','I'),
-                     "5": ('J','K','L'),
-                     "6": ('M','N','O'),
-                     "7": ('P','Q','R','S'),
-                     "8": ('T','U','V'),
-                     "9": ('W','X','Y','Z')}
-
-word_list_full = words.words() # 236,736 words
-
-# for now we need to parse out all words longer than 7 letters, also capitalize everything
-word_list_seven_letter_max_tmp = [word.upper() for word in word_list_full if len(word) <= 7]  
-word_list_seven_letter_max_tmp.sort() # makes sure list is in alphabetical order
-#print len(word_list_seven_letter_max) # 59,458 words
-
-# remove duplicates
-# using list comprehension 
-# https://www.geeksforgeeks.org/python-ways-to-remove-duplicates-from-list/
-word_list_seven_letter_max = [] 
-[word_list_seven_letter_max.append(word) for word in word_list_seven_letter_max_tmp if word not in word_list_seven_letter_max] 
+# this version only does wordifications in English
+# this version uses a word list, based on the natural language proccessing tool kit's english word list
+import phone_to_words_fx as ptw_fx
+from phone_to_words_dicts import numeric_to_alpha_dict
 
 
-def is_number_valid(digits_list):
-    # tests the digits list for validity, returns True of False
-    # for now, only test if phone number is 7, 10, or 11 digits long
-    # other conditions could be implemented
-    len_digits_list = len(digits_list)
-    if (len_digits_list != 7 and \
-        len_digits_list != 10 and \
-        len_digits_list != 11):
-        print 'phone number seems invalid, length is not 7, 10, or 11 digits.\n'
-        return False
-    else:
-        return True
-    
+# nltk_eng_words_3to7letters.txt is a word list of 3-7 letter english words
+# nltk is python's natural language processing tool kit module
 
-def preparation_routine(phone_number_string):
-    # checks if the number is valid
-    # converts the format into a list of integers
-    # separates pre-fix/area code from 7 digit phone number
-    # returns either None or two lists of integers: prefix_string, seven_digits_list
-    
-    # remove all non-numerics from the string
-    # using regex according to: https://stackoverflow.com/a/17337613
-    number_string = re.sub('[^0-9]','', phone_number_string)
-
-    # make it into a list of digits for compatibility with the code that follows
-    digits_list = [int(digit) for digit in number_string]
-    
-    # check that the number fits basic requirements, if not, returns None
-    if not is_number_valid(digits_list):
-        return None
-    
-    # divide the phone_number_string into 2 pieces
-    # 1) prefix_string: a properly formatted prefix/area code string ('1-800-', '617-'')
-    # 2) seven_digit_list: a list of seven integers used for the word searching    
-    prefix_digits_list = digits_list[:-7]
-    seven_digits_list = digits_list[-7:]
-    
-    if len(prefix_digits_list) == 0:
-        prefix_string = ''
-    elif len(prefix_digits_list) == 3:
-        prefix_string = '({})-'.format(''.join(str(d) for d in prefix_digits_list))
-    elif len(prefix_digits_list) == 4:
-        prefix_string = '{}-{}-'.format(str(prefix_digits_list[0]),
-                                          ''.join(str(d) for d in prefix_digits_list[1:]))
-    else:
-        print "the prefix/area-code has incorrect formatting"
-        return None
-    
-    return prefix_string, seven_digits_list
-
-
-
-def word_searcher(letters_lists):
-    # takes a list of lists of letter options ex: [['A','B','C'],['W','X','Y','Z'],...]
-    # returns a list of possible words using one character from each list entry, in order
-    
-    # the current dictionary i am using, set it to words_list,
-    # words list will then be trimmed down
-    words_list = word_list_seven_letter_max
-    
-    # first delete all words without length != len(letters_list)
-    # i.e. we are searching for words with a specific length
-    word_length = len(letters_lists)    
-    words_list = filter(lambda x: len(x) == word_length, words_list)
-    
-    # iteratively trim down the words_list, keeping only words matching the allowed criteria at each index
-    for i in range(word_length):
-        words_list = [filter(lambda x: x[i] == letter, words_list) for letter in letters_lists[i]]
-        words_list = [item for sub_list in words_list for item in sub_list] #flattened list
- 
-    return words_list
-
+# read in the 3-7 letter word list, 1 and 2 letter word lists were imported from phone_to_words_dicts
+in_file = open("nltk_eng_words_3to7letters.txt", 'r')
+in_lines = in_file.readlines()
+word_list_primary = tuple(line.rstrip() for line in in_lines)
+in_file.close()
 
 
 def number_to_words(phone_number_string):
     # takes input as a string
     # cleans the string into only numbers, transforms it into a list of integers
     # runs through a word searching routine
-    # returns a list of possible single-word or multiple-word options for the number
-    
-    # for now, the code only works for 7-digit words!!!
-    
-    prep_data = preparation_routine(phone_number_string)
+    # returns a list of all possible single-word or multiple-word options for the number
+
+    prep_data = ptw_fx.preparation_routine(phone_number_string)
     if not prep_data:
         print "input string does not resemble U.S. phone-number."
         return None
@@ -137,46 +37,26 @@ def number_to_words(phone_number_string):
 
     # we now have 1) prefix_string, which is a string like '', '1-800-', '(617)-'
     #             2) seven_digit_list, in the form [6,1,7,8,5,2,5,2,8,9]
-    # prefix_string is used to construct the output string at the end of this function
-    # seven_digit_list is the data we need to search for words
-    
-    # construct a list of lists from the digits
-    # the sub_lists are the associated letters for a given digit
-    # None is returned for digits 0 and 1 (see definition of alphanumeric_dict)
-    # ex: 8670000 yields [['T','U','V'], ['M','N','O'], ..., None]
-    letter_options = [alphanumeric_dict.get(str(digit)) for digit in seven_digit_list]
-    
-    # check to be sure there were no 0 or 1 in the number
-    if not all(letter_options):
-        # enters here if there are None/False-y values in the list
-        print "not all digits in phone number correspond to letters"
-        print "no contiguous word is associated with the number"
-        return None                                                  
-    
-    # start by looking for seven letter words
-    # then 6-1 split
-    # then 5-2 split
-    # then 4-3 split
-    # then 3-4 split
-    # then 2-5 split
-    # then 1-6 split
+    # prefix_string is saved to construct the output string at the end of this function
+    # it is not the intention of this code to search for wordifications with prefix numbers in the basis
+    # in the following code, seven_digit_list comprises the numerical data used to search for words
 
-    # do the same sort of procedure for up to 7 single-letter words
-    # (for example 222-2222 should return [aaa-aaaa])
-    
-    valid_words_list = word_searcher(letter_options) 
-    #print valid_words_list
-    
-    if len(valid_words_list) > 0:
-           wordifieds_list = [prefix_string + x for x in valid_words_list]
-    else:
-        print 'no wordification found.'
-        return 'no 7-letter wordification.'
-    
-    return wordifieds_list[0]                              
+    # construct a list, letter_options, with element type:
+    # ex: ['A', 'B', 'C'] corresponding to digit 2
+    # ex: 8600 yields [['T','U','V'], ['M','N','O'], [], []]
+    letter_options = [numeric_to_alpha_dict.get(str(digit)) for digit in seven_digit_list]
 
-           
-example_number_str = '(617)-724-6837'
-wordified_number = number_to_words(example_number_str)
-print wordified_number
+    # return_word_slices takes in letter_options and a list of strings
+    # it returns a list with every possible word and the associated index as a tuple
+    # (2, 'DOG')  = DOG can be formed using the 3rd, 4th, & 5th digits of the number
+    index_word = ptw_fx.return_single_word_slice(letter_options, word_list_primary)  # expects input in list form
+
+    # transform to outstring format
+    wordified_result = ptw_fx.transform_to_outstring([index_word], seven_digit_list, prefix_string)
+    return wordified_result
+
+
+ex_number = '1-800-724-6837'
+wordified_string = number_to_words(ex_number)
+print wordified_string
 
--- phone_to_words_fx.py
+++ phone_to_words_fx.py
@@ -3,15 +3,13 @@ from phone_to_words_dicts import one_letter_eng_words
 from phone_to_words_dicts import two_letter_eng_words
 
 
-
-
 def is_number_valid(digits_list):
     # tests the digits list for validity, returns True of False
     # for now, only test if phone number is 7, 10, or 11 digits long
     # other conditions could be implemented
     len_digits_list = len(digits_list)
-    if (len_digits_list != 7 and \
-            len_digits_list != 10 and \
+    if (len_digits_list != 7 and
+            len_digits_list != 10 and
             len_digits_list != 11):
         print 'phone number seems invalid, length is not 7, 10, or 11 digits.\n'
         return False
@@ -63,10 +61,8 @@ def word_searcher(letters_lists, english_words_list):
     # special words lists for ! letter and 2 letter words are used to save time
     if word_length == 1:
         words_list = one_letter_eng_words
-        #print one_letter_eng_words
     elif word_length == 2:
         words_list = two_letter_eng_words
-        #print two_letter_eng_words
     else:
         words_list = english_words_list
         words_list = filter(lambda x: len(x) == word_length, words_list)
@@ -105,15 +101,49 @@ def return_word_slices(letter_options_list, english_words_list):
     return indeces_and_words
 
 
+def return_single_word_slice(letters_list, english_words_list):
+    # takes in a list of letter options ex: [['T','U','V'], ['M','N','O'], ..., None]
+    # english_words_list is a list of all english words, perhaps somewhat pre-filtered and sorted...
+    # ... usually allowing max word length 7 for our purposes
+    # returns a single tuple of type:
+    # (0, 'PAINTER')   which corresponds to the result 1-800-PAINTER
+    # ex: (3,'OVER') transforms 1-800-724-6837  --> 1-800-724-OVER
+
+    # single word slice returns the longest word that appears alphabetically first
+    # (i.e. length is top priority, then the alphabet is searched in the a->z direction
+
+    # takes in a list of letter options ex: [['T','U','V'], ['M','N','O'], ..., None]
+    # english_words_list is a list of all english words, perhaps somewhat pre-filtered and sorted...
+    # ... usually allowing max word length 7 for our purposes
+    # returns all possible word slices along with their starting index 0-6 (0-length)
+
+    len_list = len(letters_list)
+    # 3 being index of the word, it begins at the 4th digit
+    for i in reversed(range(len_list)):
+        slots_to_grab = i + 1
+        index_f = len_list
+        while index_f - slots_to_grab >= 0:
+            index_i = index_f - slots_to_grab
+            tmp_letter_options = letters_list[index_i:index_f]
+
+            # now perform the word search for this slice
+            tmp_words = word_searcher(tmp_letter_options, english_words_list)
+            if len(tmp_words) != 0:
+                index_word = (index_i, tmp_words[0])   # of type (3, 'DOG')
+                return index_word
+            index_f -= 1
+
+    # is nothing returned in the loop, then no woridifications were found
+    return None
+
+
 def transform_to_outstring(word_combo, seven_digit_list, prefix_string=''):
     # still needs some comments to explain the function  :)
 
     out_chars = seven_digit_list[:]
 
-    previous_end_i = -1
     for word in word_combo:
         word_i, word_string = word
-        word_len = len(word_string)
 
         # replace the numbers in the digit list appropriately with letter or dashes
         # conditional statements solve a host of small formatting issues
@@ -121,34 +151,22 @@ def transform_to_outstring(word_combo, seven_digit_list, prefix_string=''):
             tmp_i = i + word_i  # word_i is the starting position of the word
             if i == 0 and tmp_i != 0:
                 replacement_char = '-' + char
-
-
-            #if tmp_i - 1 == previous_end_i:
-            #    replacement_char = char
-            #elif tmp_i == 6:  # fixes 1-800-PAINTER-
-            #    replacement_char = char
-
-            #elif i == word_len - 1:
-            #    replacement_char = char + '-'
             else:
                 replacement_char = char
             out_chars[tmp_i] = replacement_char
-        previous_end_i = word_i + word_len - 1
 
     # https://stackoverflow.com/a/3590175
     out_string = prefix_string + ''.join(str(i) for i in out_chars)
 
-
-    #final clean up, places '-' between letter-number errors [A-Z][0-9] (ex. 1-800-7-A468-ER)
+    # final clean up, places '-' between letter-number errors [A-Z][0-9] (ex. 1-800-7-A468-ER)
     def formatter(text, index):
-        i = index
-        if i + 1 == len(text):
-            return text[i]
-        if text[i].isalpha() & text[i + 1].isdigit():
-            return text[i] + '-'
+        j = index
+        if j + 1 == len(text):
+            return text[j]
+        if text[j].isalpha() & text[j + 1].isdigit():
+            return text[j] + '-'
         else:
-            return text[i]
-
+            return text[j]
     out_string = ''.join([formatter(list(out_string), i) for i, char in enumerate(list(out_string))])
 
     return out_string
